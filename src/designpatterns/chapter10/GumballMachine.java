79305efc5736c96cbe4ebe9b040860e85f240d60
Pavel Claudiu Stefan <pavelclaudiu96@gmail.com>
2018-07-23 18:47:55 +0300

    Added chapter 10

---
+++ src/designpatterns/chapter10/GumballMachine.java
@@ -0,0 +1,86 @@
+package designpatterns.chapter10;
+
+public class GumballMachine {
+
+    private State noQuarterState;
+    private State hasQuarterState;
+    private State soldState;
+    private State soldOutState;
+    private State winnerState;
+
+    private State state = soldOutState;
+    int count = 0;
+
+    public GumballMachine(int numberOfGumballs) {
+        noQuarterState = new NoQuarterState(this);
+        hasQuarterState = new HasQuarterState(this);
+        soldState = new SoldState(this);
+        soldOutState = new SoldOutState(this);
+        winnerState = new WinnerState(this);
+        this.count = numberOfGumballs;
+        if (numberOfGumballs > 0) {
+            state = noQuarterState;
+        }
+    }
+
+    public void insertQuarter() {
+        state.insertQuarter();
+    }
+
+    public void ejectQuarter() {
+        state.ejectQuarter();
+    }
+
+    public void turnCrank() {
+        state.turnCrank();
+        state.dispense();
+    }
+
+    public void releaseBall() {
+        System.out.println("A gumball comes rolling out the slot...");
+        if (count > 0) {
+            count--;
+        }
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public State getNoQuarterState() {
+        return noQuarterState;
+    }
+
+    public State getHasQuarterState() {
+        return hasQuarterState;
+    }
+
+    public State getSoldState() {
+        return soldState;
+    }
+
+    public State getSoldOutState() {
+        return soldOutState;
+    }
+
+    public State getWinnerState() {
+        return winnerState;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void refill(int count) {
+        this.count = count;
+        state = noQuarterState;
+    }
+
+    @Override
+    public String toString() {
+        return  "\nMighty Gumball, Inc." +
+                "\nJava-enabled Standing Gumball Model #2018" +
+                "\nInventory: " + count + " gumballs" +
+                "\n" + state.toString() + "\n";
+    }
+}
---
+++ src/designpatterns/chapter10/HasQuarterState.java
@@ -0,0 +1,47 @@
+package designpatterns.chapter10;
+
+import java.util.Random;
+
+public class HasQuarterState implements State {
+
+    Random randomWinner = new Random(System.currentTimeMillis());
+    GumballMachine gumballMachine;
+
+    public HasQuarterState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    @Override
+    public void insertQuarter() {
+        System.out.println("You can't insert another quarter");
+    }
+
+    @Override
+    public void ejectQuarter() {
+        System.out.println("Quarter returned");
+        gumballMachine.setState(gumballMachine.getNoQuarterState());
+    }
+
+    @Override
+    public void turnCrank() {
+        System.out.println("You turned...");
+        int winner = randomWinner.nextInt(10);
+        if ((winner == 0) && (gumballMachine.getCount() > 1)) {
+            System.out.println("Winner test " + winner);
+            gumballMachine.setState(gumballMachine.getWinnerState());
+        } else {
+            System.out.println("Sold test " + winner);
+            gumballMachine.setState(gumballMachine.getSoldState());
+        }
+    }
+
+    @Override
+    public void dispense() {
+        System.out.println("No gumball dispensed");
+    }
+
+    @Override
+    public String toString() {
+        return "Machine has a quarter";
+    }
+}
